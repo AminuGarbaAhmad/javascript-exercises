e9f662b30a8dc710951846ddd1f8ba663c8d3d67
AminuTechs <agadkna123@gmail.com>
2026-01-01 19:17:49 +0100

    solution one from JS file one done

--- exercise1.js
+++ exercise1.js
@@ -4,5 +4,5 @@ let firstName = "John"
 let lastName = "Doe"
 let age = 25
 
-console.log("My name is $firstName $lastName and I am $age years old.");
+console.log("My name is", firstName, lastName, "and I am", age, "years old.");
 
