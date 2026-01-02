e3e2cea71917bbd98334ce65d0cddf6ca1479511
AminuTechs <agadkna123@gmail.com>
2026-01-02 01:13:58 +0100

    exercise1.js solution done

--- exercise1.js
+++ exercise1.js
@@ -6,3 +6,47 @@ let age = 25
 
 console.log("My name is", firstName, lastName, "and I am", age, "years old.");
 
+//Question two solution:
+
+let num1 = 15
+let num2 = 8
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+
+
+//Question three solution:
+
+let temperature = 22
+
+if (temperature < 20) {
+    console.log("It's cold");
+    
+} else {
+    console.log("It's Warm");
+}
+
+
+//Question four solution:
+
+let price = 50
+let discount = 10
+
+console.log(price - discount);
+
+
+//Question five solution:
+
+let number = 1
+
+if (number > 0) {
+    console.log("positive number");
+    
+}else if (number < 0) {
+     console.log("negative number");
+    
+}else if (number == 0) {
+     console.log("Zero");
+}
