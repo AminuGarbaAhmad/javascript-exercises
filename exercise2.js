b036454e9ed0763437c264c3727c66ba03a143c7
AminuTechs <agadkna123@gmail.com>
2026-01-06 16:40:14 +0100

    exercise2.js solution done

--- exercise2.js
+++ exercise2.js
@@ -8,3 +8,54 @@ if (score >= 50) {
 } else {
     console.log("Fail");
 }
+
+
+//QUESTION SEVEN SOLUTION
+let a = 12
+let b = 15
+
+if (a>b) {
+    console.log("a is greater than b");
+    
+} else {
+    console.log("a is less than b");
+}
+
+
+//QUESTION EIGHT SOLUTION
+
+
+let age = 16
+
+if (age >= 18) {
+    console.log("Can vote");
+
+} else {
+    console.log("Cannot vote");
+}
+
+
+
+//QUESTION NINE SOLUTION
+
+let isWeekend = true;
+
+if (isWeekend === true) {
+    console.log("No work today!");
+
+} else {
+    console.log("Time to work!");
+}
+
+
+
+//QUESTION TEN SOLUTION
+
+let num = 24
+
+if (num % 2 == 0) {
+    console.log("Even!");
+
+} else {
+    console.log("Odd");
+}
